fedeb54236350031f0ef2f13368ca48049f2cae7
Noman46 <nomanibrahim7@gmail.com>
2024-06-15 11:29:48 +0600

    mock first one

---
+++ test/specs/Mock/test.mock.prac.js
@@ -0,0 +1,8 @@
+
+describe('Mock Practise', () => {
+   
+    it("Mocking", async ()=>{
+        await browser.url('https://www.selenium.dev/selenium/web/alerts.html#')
+        await browser.pause(3000);
+    })
+})
